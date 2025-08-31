9e7e71b27406eaf494b5249d5248a68219be6229
Ambuj Shrivastav <ambujshri4@gmail.com>
2025-08-31 21:54:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Optional JavaScript (you can skip this)
-console.log("Portfolio Loaded!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
